ab32b52d46cdf2578a6590b36567e61177ebc450
marcelo cordeiro <hi@marcelo.berlin>
2022-09-24 13:51:15 +0200

    Add a list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './index.scss';
 
 const title = 'Blog'
+const comments = [
+    { id: 1, title: 'Awesome'},
+    { id: 2, title: 'Great'},
+    { id: 3, title: 'Amazing'},
+    { id: 4, title: 'Ridiculous'},
+    { id: 5, title: 'Nice'},
+]
 
 function App() {
     return (
         <>
             <h1>{title}</h1>
-            <p>Hi</p>
-            { Math.random() * 5 + 5 }
+            <p>Comments ({comments.length})</p>
+            <ul>
+                {comments.map((comment) => (
+                    <li key={comment.id}>{comment.title}</li>
+                ))}
+            </ul>
         </>
     )
 }
