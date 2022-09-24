5102a377c88d11c0592583cb4ed26468339daf5e
marcelo cordeiro <hi@marcelo.berlin>
2022-09-24 17:42:24 +0200

    Fix indentation as for eslint

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
 
-const title = 'Blog';
-const comments = [
-    { id: 1, title: 'Awesome'},
-    { id: 2, title: 'Great'},
-    { id: 3, title: 'Amazing'},
-    { id: 4, title: 'Ridiculous'},
-    { id: 5, title: 'Nice'},
-];
+  const title = 'Blog';
+  const comments = [
+    { id: 1, title: 'Awesome' },
+    { id: 2, title: 'Great' },
+    { id: 3, title: 'Amazing' },
+    { id: 4, title: 'Ridiculous' },
+    { id: 5, title: 'Nice' },
+  ];
 
-const loading = false;
-const showComments = true;
+  const loading = false;
 
-const commentBlock = (
-                    <>
-                        <p>Comments ({comments.length})</p>
-                        <ul>
-                            {comments.map((comment) => (
-                                <li key={comment.id}>{comment.title}</li>
-                            ))}
-                        </ul>
-                    </>
-)
+  const [comment, setComment] = useState(false);
 
-if (loading) return <h1>Loading</h1>
+  const commentBlock = (
+    <>
+      <p>Comments ({comments.length})</p>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.title}</li>
+        ))}
+      </ul>
+    </>
+  )
 
-    return (
-        <>
-            <h1>{title}</h1>
-            {showComments ? commentBlock : "no comments"}
-        </>
-    )
+  if (loading) return <h1>Loading</h1>
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <button onClick={() => setComment(!comment)}>{comment ? 'Hide' : 'Show'} Comment</button>
+      <div>
+        {comment ? commentBlock : "no comments"}
+      </div>
+    </>
+  )
 }
 
 export default App
