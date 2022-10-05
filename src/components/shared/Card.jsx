649abb803bfed4928582d90d589503a26c26d240
marcelo cordeiro <hi@marcelo.berlin>
2022-10-05 09:56:31 +0200

    Define proptypes of card

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,7 +1,16 @@
+import PropTypes from 'prop-types';
+
 function Card({reverse, children}) {
   return (
-    <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+    <div className={`card ${reverse && 'reverse'}`}>
+      {children}
+    </div>
   )
+}
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  reverse: PropTypes.bool,
 }
 
 export default Card
